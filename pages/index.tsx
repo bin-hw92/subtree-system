617417382158f9b2480c8234bea58397758ee03e
빈현욱 <bhin@binhyeon-ug-ui-MacBookAir.local>
2024-06-04 09:15:20 +0900

    Add existing packages to their respective subtrees

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Button from "common/src/components/Button";
+import React from "react";
+const Home: React.FC = () => (
+  <div>
+    <h1>Concierge Page</h1>
+    <Button label="Click Me" />
+  </div>
+);
+
+export default Home;
